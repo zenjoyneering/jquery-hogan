dfa45ee937567def12632a9542b86a6980192758
Arthur Orlov <wirewit@yandex.ru>
2012-02-24 14:52:06 +0400

    Simple and primitive stats

--- jquery.hogan.js
+++ jquery.hogan.js
@@ -3,3 +3,22 @@
 
     @author wirewit@yandex.ru
 */
+jQuery.fn.hogan = function(context){
+    return this.each(function(){
+        var $this = $(this),
+            template = $this.data("hogan");
+        if (!template) {
+            // Replacement allows to use template expressions inside the 
+            // href attributes, which is escaped in jQuery.html
+            $this.data(
+                "hogan", 
+                Hogan.compile(
+                    $this.html().replace(/%7B%7B/g, '{{')
+                                .replace(/%7D%7D/g, '}}')
+                )
+            );
+            template = $this.data("hogan");
+        }
+        $this.html(template.render(context));
+    });
+}
